0e4616b3da2dd347bdaa22ca45bba01c2792f0a5
hillaryme <tenskap@gmail.com>
2022-08-04 09:35:58 +0300

    add user model

---
+++ app/models/user.server.ts
@@ -0,0 +1,39 @@
+import type { User } from "@prisma/client";
+import { prisma } from "~/db.server";
+import bcrypt from "bcryptjs";
+import gravatar from "gravatar";
+
+export async function getUserById(id: User["id"]) {
+   return prisma.user.findUnique({ where: { id }});
+}
+
+export async function getUserByEmail(email: User["email"]) {
+   return prisma.user.findUnique({ where: { email }});
+}
+
+export async function createUser(
+   email: User["email"],
+   password: User["password"],
+   name: User["name"]
+) {
+   const hashedPassword = await bcrypt.hash(password, 10);
+
+   // get users gravatar
+   const avatar = gravatar.url(email, {
+      s: "200",
+      r: "pg",
+      d: "mm"
+   })
+
+   return prisma.user.create({
+      data: {
+         name,
+         email,
+         password: hashedPassword,
+         avatar
+      }
+   })
+}
+
+export type { User } from "@prisma/client";
+
