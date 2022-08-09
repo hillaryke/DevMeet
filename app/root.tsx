01ad4e1a3853f172f2694cae1a4ef58ff0a3ff16
hillaryme <tenskap@gmail.com>
2022-08-09 07:46:18 +0300

    Catch unexpected errors

--- app/root.tsx
+++ app/root.tsx
@@ -51,3 +51,14 @@ export default function App() {
     </html>
   );
 }
+
+export function ErrorBoundary({ error }: { error: Error }) {
+   return (
+      <div>
+         <h1>Error</h1>
+         <p>{error.message}</p>
+         <p>The stack trace is:</p>
+         <pre>{error.stack}</pre>
+      </div>
+   );
+}
