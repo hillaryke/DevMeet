7a28b6dcddf56f13709ddd6a0bc847084ad6ab5c
Hillary <tenskap@gmail.com>
2023-03-04 17:28:36 +0300

    Add messenger chat plugin

--- app/root.tsx
+++ app/root.tsx
@@ -49,6 +49,30 @@ export default function App() {
          <Links/>
       </head>
       <body className="h-full font-Railway bg-sky-7">
+         <!-- Load Facebook SDK for JavaScript -->
+<div id="fb-root"></div>
+<script>
+window.fbAsyncInit = function() {
+  FB.init({
+    xfbml            : true,
+    version          : 'v16.0'
+  });
+};
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+<!-- Your Chat Plugin code -->
+<div class="fb-customerchat"
+  attribution="install_email"
+  attribution_version="biz_inbox"
+  page_id="103713732669836">
+</div>
       <Headerr/>
       <Outlet/>
     <ScrollRestoration/>
